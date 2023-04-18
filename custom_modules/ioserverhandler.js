ebe02bc87d4904ce646a192985d7885c1e76281b
xuoxod <xuoxod@gmail.com>
2023-04-18 16:34:50 -0400

    Minor update

--- custom_modules/ioserverhandler.js
+++ custom_modules/ioserverhandler.js
@@ -160,14 +160,16 @@ export default (io) => {
 
           if (addedUser) {
             log(`User ${regUser.fname} successfully updated\n\n`);
-            const unblockedByUser = userManager.getUser(unblockedBy);
 
             io.emit("updateonlineuserlist", {
               users: stringify(userManager.getUsers()),
             });
 
-            if (unblockedByUser) {
-              io.to(unblockedByUser.sid).emit("clearunblocked");
+            if (unblockedBy) {
+              const unblockedByUser = userManager.getUser(unblockedBy);
+              if (unblockedByUser) {
+                io.to(unblockedByUser.sid).emit("clearunblocked");
+              }
             }
           }
         }
