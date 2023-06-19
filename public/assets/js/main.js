3dd7c845953488949470c523d77e09988ea69b29
xuoxod <xuoxod@gmail.com>
2023-06-19 13:15:13 -0400

    Modified layout

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,23 +1,8 @@
 import { log } from "./clientutils.js";
 
-if (window) {
-  const nav = navigator;
-  const platform = nav.platform;
-  const lang = nav.language;
-  const langs = nav.languages;
-  const userAgent = nav.userAgent;
-  const vendor = nav.vendor;
+if ( window ) {
 
-  log(`\nPlatform:\t${platform}`);
-  log(`Languages:\t${langs}`);
-  log(`Language:\t${lang}`);
-  log(`UAgent:\t${userAgent}`);
-  log(`Vendor:\t${vendor}`);
-  log(
-    `--------------------------------------------------------------------\n\n`
-  );
-
-  addEventListener("beforeunload", (event) => {
-    log(`\n\tBefore unload\n`);
-  });
+  addEventListener( "beforeunload", ( event ) => {
+    log( `\n\tBefore unload\n` );
+  } );
 }
